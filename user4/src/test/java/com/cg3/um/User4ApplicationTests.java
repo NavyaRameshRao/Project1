e3388b3272239817f3a682d20d66900e256423a4
arvin <arvin@ARVIND>
2023-10-14 19:01:57 +0530

    first commit

---
+++ user4/src/test/java/com/cg3/um/User4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg3.um;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class User4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
